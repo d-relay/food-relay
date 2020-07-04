385780d450e4a59a3650d528bb5c060b5d224f4c
X-tymchenko <xtymchenko@ex.ua>
2020-07-04 19:12:47 +0300

    do some trash

--- bin/www.ts
+++ bin/www.ts
@@ -1,27 +1,16 @@
 import app from '../app';
 import debug from 'debug';
-import http from 'http';
+import http2 from 'http2';
 
 debug('relay:server');
 
 const port: number = normalizePort(process?.env?.PORT);
-// app.set('port', port);
 
-/**
- * Create HTTP server.
- */
 
-// const server = http2.createServer({
-//   key: fs.readFileSync('localhost-privkey.pem'),
-//   cert: fs.readFileSync('localhost-cert.pem')
-// }, app);
-
-var server = http.createServer(app.callback());
-
-server.listen(port, () => console.log('listen ' + port));
-server.on('error', onError);
-server.on('listening', onListening);
-
+const serverHttp2 = http2.createServer(app.callback());
+serverHttp2.listen(port, () => console.log('listen ' + port));
+serverHttp2.on('error', onError);
+serverHttp2.on('listening', onListening2);
 
 function normalizePort(val: string | undefined) {
   if (!val) return 5000;
@@ -55,8 +44,8 @@ function onError(error: any) {
   }
 }
 
-function onListening() {
-  const addr = server.address();
+function onListening2() {
+  const addr = serverHttp2.address();
   const bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr?.port;
