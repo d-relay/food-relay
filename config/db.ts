aed19dce61dc61733872e846b63f30fb2e4ff811
x-ror <16686943+x-ror@users.noreply.github.com>
2020-12-04 00:44:47 +0200

    env fix

--- config/db.ts
+++ config/db.ts
@@ -2,9 +2,9 @@ import path from 'path'
 import { createConnection } from 'typeorm'
 createConnection({
 	type: 'postgres',
-	url: process.env.TYPEORM_URL,
+	url: process.env.DATABASE_URL,
 	synchronize: false,
-	logging: false,
+	logging: true,
 	uuidExtension: 'pgcrypto',
 	entities: [
 		path.join(__dirname, '../entities/**.ts')
