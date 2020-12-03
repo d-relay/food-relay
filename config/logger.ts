0e5266a9ffc4f29c288ddd5f90cd54b3e02701ee
x-ror <16686943+x-ror@users.noreply.github.com>
2020-12-04 00:06:39 +0200

    add rollbar

--- config/logger.ts
+++ config/logger.ts
@@ -1,10 +1,19 @@
 import { createStream } from 'rotating-file-stream'
+import Rollbar from "rollbar"
+
+const rollbar = new Rollbar({
+	accessToken: '7d33204e7ee247579fb0163e54f6531c',
+	captureUncaught: true,
+	captureUnhandledRejections: true
+})
 
 export const accessLogStream = createStream(generator.bind({ filename: 'accessLogStream.log' }), { size: '10M', interval: '1M' })
 export const errorBackendLogStream = createStream(generator.bind({ filename: 'errorBackendLogStream.log' }), { size: '10M', interval: '1M' })
 
-function generator (this: { filename: string }, time: number | Date): string {
+function generator(this: { filename: string }, time: number | Date): string {
 	if (!time) return 'logs/' + this.filename
 	const date = new Date(time).toLocaleDateString()
-	return `logs/${date}/${date}-${this.filename}`
+	const log = `logs/${date}/${date}-${this.filename}`;
+	rollbar.log(log);
+	return log;
 };
