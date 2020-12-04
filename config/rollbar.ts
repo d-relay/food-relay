b735d134975583f3dc65568dad3def21beb0768e
x-ror <16686943+x-ror@users.noreply.github.com>
2020-12-04 13:31:41 +0200

    add env logs

--- config/rollbar.ts
+++ config/rollbar.ts
@@ -1,2 +1,5 @@
 import Rollbar from "rollbar"
-export const rollbar = new Rollbar({ accessToken: process.env.ROLLBAR_ACCESS_TOKEN });
+export const rollbar = new Rollbar({
+    accessToken: process.env.ROLLBAR_ACCESS_TOKEN,
+    environment: process.env.NODE_ENV
+});
