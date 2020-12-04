08ed9bfd516ec48ce794ff82d50d252ea94fc229
x-ror <16686943+x-ror@users.noreply.github.com>
2020-12-04 13:05:40 +0200

    convert to string

--- routes/Auth.ts
+++ routes/Auth.ts
@@ -6,7 +6,7 @@ const router = new Router()
 
 router.post('/login', async (ctx, next) => {
 	const { id: client_id, login, email, accessToken } = (<any>ctx.request).body
-	rollbar.info({ client_id, login, email, accessToken });
+	rollbar.info(`client_id: "${client_id}". login: "${login}". email: "${email}". accessToken: "${accessToken}"`);
 
 	const userServises = new UserServices()
 	const user: User = await userServises.FindByCliendId({ client_id })
