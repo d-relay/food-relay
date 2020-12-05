a8beb896a5ce25305cbea642c5e0c68d8deb1dec
x-ror <16686943+x-ror@users.noreply.github.com>
2020-12-05 10:35:13 +0200

    remove Socket type. use parse method

--- config/socket-map.ts
+++ config/socket-map.ts
@@ -2,24 +2,24 @@ import url from 'url'
 import http from 'http'
 import debug from 'debug'
 import { Server } from 'ws'
+import { rollbar } from './rollbar'
 
 export let socketServer: Server
 
 export function createSocketConnetion(server: http.Server) {
 	socketServer = new Server({ server })
 
-	socketServer.on('connection', (socket, req) => {
+	socketServer.on('connection', (socket: any, req) => {
 		try {
-			const token = new url.URL(req.url).href.substring(1)
-			type Socket = typeof socket & { _token: string };
+			const token = url.parse(req.url).href.split('/')[1];
 			if (token) {
-				(socket as Socket)._token = token
+				socket._token = token
 				socket.on('close', () => {
 					debug('Client disconnected')
 				})
 			}
 		} catch (error) {
-
+			rollbar.error(error);
 		}
 	})
 }
