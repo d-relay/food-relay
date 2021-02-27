0287bf03452e6d8d119957403edb689646e13ef8
x-ror <16686943+x-ror@users.noreply.github.com>
2021-02-27 22:40:00 +0200

    fix request

--- services/PartnerServices.ts
+++ services/PartnerServices.ts
@@ -3,6 +3,6 @@ import { getManager } from 'typeorm'
 export class PartnerServices {
 	constructor(private manager = getManager()) { }
 	async loadPartners(): Promise<any> {
-		return this.manager.query('SELECT p.name, p.min_limit, i.filename FROM partner as p inner join image as i on p.id = i.imageableId');
+		return this.manager.query('SELECT p.name, p.min_limit, i.filename FROM partner as p inner join image as i on p.id = i."imageableId"');
 	}
 }
